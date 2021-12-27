8337420c253de57ce10cc527621a2fc1f0a76540
mfhw20 <mfhw20@gmail.com>
2021-12-27 12:37:10 +0100

    Initial commit

---
+++ src/win32_wrappers.h
@@ -0,0 +1,17 @@
+//
+// Wizard-2020 Example from https://building.enlyze.com/posts/writing-win32-apps-like-its-2020-part-1
+// Copyright (c) 2020 Colin Finck, ENLYZE GmbH
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+
+static inline auto make_unique_font(HFONT hFont)
+{
+    return sr::make_unique_resource_checked(hFont, nullptr, DeleteObject);
+}
+
+static inline auto make_unique_handle(HANDLE h)
+{
+    return sr::make_unique_resource_checked(h, INVALID_HANDLE_VALUE, CloseHandle);
+}
